c5eec65c0fab933f5a1b2ca5c93c5196eb5847f3
saumya-mendiratta <saya.mendiratta@gmail.com>
2020-10-13 00:42:35 +0530

    Update armstrong.java

--- armstrong.java
+++ armstrong.java
@@ -6,43 +6,43 @@ public class armstrong {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-		
+
 		int lo = scn.nextInt();
 		int hi = scn.nextInt();
-		
-      //  int c=digits(lo);
-        // System.out.println(c);
+
+		// int c=digits(lo);
+		// System.out.println(c);
 		printarmstrong(lo, hi);
-		//int d=sum(lo);
-		//System.out.println(d);
+		// int d=sum(lo);
+		// System.out.println(d);
 
 	}
-	
-	public static int digits(int n){
-		int count=0;
-		int a=1;
-		while(a!=0)
-		{
-			a=n%10;
-			n=n/10;
+
+	public static int digits(int n) {
+		int count = 0;
+		int a = 1;
+		while (a != 0) {
+			a = n % 10;
+			n = n / 10;
 			count++;
 		}
 		return (--count);
 	}
-	
-	public static int sum(int n){
-		int sum=0;
-		int a=1;
-		int b=digits(n);
-		while(a!=0)
-		{ a=n%10;
-		
-			sum+=Math.pow(a,b);
-			n=n/10;
+
+	public static int sum(int n) {
+		int sum = 0;
+		int a = 1;
+		int b = digits(n);
+		while (a != 0) {
+			a = n % 10;
+
+			sum += Math.pow(a, b);
+			n = n / 10;
 		}
 		return sum;
-		
+
 	}
+
 	public static void printarmstrong(int lo, int hi) {
 		for (int i = lo; i <= hi; i++) {
 			boolean res = isarmstrong(i);
@@ -50,17 +50,14 @@ public class armstrong {
 				System.out.println(i);
 		}
 	}
+
 	public static boolean isarmstrong(int n) {
-		
-		int b=sum(n);
-		if(b==n)
+
+		int b = sum(n);
+		if (b == n)
 			return true;
-		else 
+		else
 			return false;
 	}
-	
-	
-	
-	
-	
+
 }
