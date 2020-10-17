44e9d9aadca00151204e52f79f1d65848d01cdf8
saumya-mendiratta <saya.mendiratta@gmail.com>
2020-10-17 11:43:22 +0530

    Create subarraysum2loop.java

---
+++ subarraysum2loop.java
@@ -0,0 +1,27 @@
+package lec4;
+
+public class subarraysum2loop {
+
+	public static void main(String[] args) {
+		int [] a={10,20,30,40};
+		int n= a.length;
+		int sum=0;
+		int sum2=0;
+		
+		   for(int i=1;i<=n;i++)
+			sum+=i;
+       
+		   for (int si=0;si<n;si++)
+		   { sum2=0;
+			   for (int ei=si; ei<n;ei++)
+			   { 
+				 sum2+=a[ei];
+				 System.out.println(ei + " "+ si+ " " + sum2 );
+				  }
+				
+			   }
+			
+		   }
+		  
+	}
+
