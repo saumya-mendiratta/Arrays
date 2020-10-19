3dbe8ab70c8bef6c3979a54cc4690b45e590f34a
saumya-mendiratta <saya.mendiratta@gmail.com>
2020-10-19 06:52:22 +0530

    Update subset.java

--- subset.java
+++ subset.java
@@ -1,10 +1,18 @@
 package lec4;
 
+import java.util.Scanner;
+
 public class subset {
 
 	public static void main(String[] args) {
-		
-		int[] a = { 1, 2, 3 ,4};
+
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] a = new int[n];
+	//	int[] a = { 1, 2, 3, 4 };
+		for (int i = 0; i < a.length; i++) {
+			a[i]=scn.nextInt();
+		}
 		int limit = (int) Math.pow(2, a.length);
 		int rem = 0;
 		for (int n = 0; n <= limit - 1; n++) {
