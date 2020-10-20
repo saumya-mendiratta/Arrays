93e34221ade3d175fea7fa94e87c3412fca93d5b
saumya-mendiratta <saya.mendiratta@gmail.com>
2020-10-20 19:23:28 +0530

    Update cointoss.java

--- cointoss.java
+++ cointoss.java
@@ -5,25 +5,25 @@ import java.util.Scanner;
 public class cointoss {
 
 	public static void main(String[] args) {
-		Scanner scn= new Scanner(System.in);
-		int t=scn.nextInt();
-		int limit=(int)Math.pow(2,t);
-		int rem=0;
-		for( int n=0; n<limit; n++)
-		{int temp=n;
-			for (int i=0;i<t;i++)
-			{
-				rem=temp%2;
-				if(rem==1)
+		
+		Scanner scn = new Scanner(System.in);
+		int t = scn.nextInt();
+		int limit = (int) Math.pow(2, t);
+		int rem = 0;
+		for (int n = 0; n < limit; n++) {
+			int temp = n;
+			for (int i = 0; i < t; i++) {
+				rem = temp % 2;
+				if (rem == 1)
 					System.out.print("H");
 				else
 					System.out.print("T");
-				temp=temp/2;
+				temp = temp / 2;
 			}
+			
 			System.out.println();
-			//temp=temp/2;
+			// temp=temp/2;
 		}
-
 
 	}
 }
