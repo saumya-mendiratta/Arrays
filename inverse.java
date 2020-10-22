8380a2a0141c235bebae7befc7a47487141e7f96
saumya-mendiratta <saya.mendiratta@gmail.com>
2020-10-22 19:59:18 +0530

    Create inverse.java

---
+++ inverse.java
@@ -0,0 +1,42 @@
+package lec4;
+
+import java.util.Scanner;
+
+public class inverse {
+
+	static Scanner scn = new Scanner(System.in);
+	public static void main(String[] args) {
+	
+		
+		int[] arr=takeinput();
+		int n= arr.length;
+		int[] b= new int[n];
+		int temp=0;
+		for(int i=0; i<n;i++)
+		{
+			temp=arr[i];
+			b[temp]=i;
+			
+		}
+		for(int j=0; j<b.length;j++)
+		{
+			System.out.print(b[j]+ " ");
+		}
+	}
+	public static int[] takeinput()
+	{
+		
+		
+		int n= scn.nextInt();
+		int[] arr= new int[n];
+		 for (int i=0; i<arr.length; i++)
+		 {
+			 
+			 int val= scn.nextInt();
+			 arr[i]=val;
+		 }
+		 return arr;
+	}
+	
+
+}
