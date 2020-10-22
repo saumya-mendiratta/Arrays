6640949de58372d6563e7e37a2d32213c513ab9b
saumya-mendiratta <saya.mendiratta@gmail.com>
2020-10-22 20:00:36 +0530

    Update inverse.java

--- inverse.java
+++ inverse.java
@@ -4,39 +4,35 @@ import java.util.Scanner;
 
 public class inverse {
 
-	static Scanner scn = new Scanner(System.in);
 	public static void main(String[] args) {
-	
+
+		 Scanner scn = new Scanner(System.in);
 		
-		int[] arr=takeinput();
-		int n= arr.length;
-		int[] b= new int[n];
-		int temp=0;
-		for(int i=0; i<n;i++)
-		{
-			temp=arr[i];
-			b[temp]=i;
-			
+		int[] arr = takeinput();
+		int n = arr.length;
+		int[] b = new int[n];
+		int temp = 0;
+		for (int i = 0; i < n; i++) {
+			temp = arr[i];
+			b[temp] = i;
+
 		}
-		for(int j=0; j<b.length;j++)
-		{
-			System.out.print(b[j]+ " ");
+		for (int j = 0; j < b.length; j++) {
+			System.out.print(b[j] + " ");
 		}
 	}
-	public static int[] takeinput()
-	{
+
+	public static int[] takeinput() {
+
+		int n = scn.nextInt();
 		
-		
-		int n= scn.nextInt();
-		int[] arr= new int[n];
-		 for (int i=0; i<arr.length; i++)
-		 {
-			 
-			 int val= scn.nextInt();
-			 arr[i]=val;
-		 }
-		 return arr;
+		int[] arr = new int[n];
+		for (int i = 0; i < arr.length; i++) {
+
+			int val = scn.nextInt();
+			arr[i] = val;
+		}
+		return arr;
 	}
-	
 
 }
