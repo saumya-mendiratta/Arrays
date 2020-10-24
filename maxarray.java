f86addea7014a11f268494acb6b2b4c85d025705
saumya-mendiratta <saya.mendiratta@gmail.com>
2020-10-24 16:30:49 +0530

    Update maxarray.java

--- maxarray.java
+++ maxarray.java
@@ -3,16 +3,19 @@ package lec4;
 public class maxarray {
 
 	public static void main(String[] args) {
-		int[] a = { -1, -2,- 3, -4, -5 };
+		
+		
+		int[] a = { -1, -2, -3, -4, -5 };
+		
 		int max = Integer.MIN_VALUE;
+		
 		for (int i = 0; i < a.length; i++) {
 			{
 				a[0] = max;
 				if (a[i] > max)
 					max = a[i];
-              
+
 			}
-			// System.out.println(max);
 		}
 		System.out.println(max);
 
