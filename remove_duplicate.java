ef0940b30db3098a9f93bba9a42f65a8581ebe2b
saumya-mendiratta <saya.mendiratta@gmail.com>
2021-05-04 00:16:56 +0530

    Create remove_duplicate.java

---
+++ remove_duplicate.java
@@ -0,0 +1,48 @@
+package self_afterarray;
+
+import java.util.Scanner;
+
+public class remove_duplicate {
+
+
+	static Scanner scn = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		String s=scn.next();
+		comp(s);
+	}
+
+	public static void comp(String s) {
+		
+		
+		char[] a=new char[s.length()];
+		int count =0;
+		for (int i = 0; i<s.length(); i+=count)
+		{
+			 count =0;
+		for(int j=i;j<s.length();j++)
+		{
+			if(s.charAt(i)==s.charAt(j))
+				count++;
+			else 
+			break;
+			
+			
+		}
+		if(count>1 )
+		{	
+			System.out.print(s.charAt(i));
+		   // System.out.print(count);
+				
+			}
+		else
+			System.out.print(s.charAt(i));
+		
+		}
+
+	
+	}
+
+}
